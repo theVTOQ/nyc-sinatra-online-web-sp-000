b90b3a601e7495d7405ee421b0602093b1d9c558
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-08-02 23:01:22 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -4,4 +4,10 @@ class LandmarksController < ApplicationController
 
     erb :"/landmarks/index"
   end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+
+    erb :"/landmarks/show"
+  end
 end
